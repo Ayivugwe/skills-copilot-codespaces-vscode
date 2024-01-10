dc80c2f19882c854a4fc39af469149c4baa2dac3
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2024-01-10 11:39:51 +0000

    add skillsMember

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    var member = {
+        name: 'John Doe',
+        age: 30,
+        address: '123 Main St'
+    };
+
+    return member;
+}   // End of skillsMember() function.  
+
+// Path
